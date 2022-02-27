bf033b90f9074e35154d205feb54d06b8fbfdd01
Matthew Gerges <mattgerges@gmail.com>
2022-02-27 14:32:24 -0800

    Added enviroment setup script

---
+++ environmentSetup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get update 
+
+sudo apt install firefox
+
+sudo apt install default-jre
+
+sudo apt install default-jdk
+
+sudo apt install blender
+
+sudo apt-get install libglu1-mesa-dev freeglut3-dev mesa-common-dev #Install OpenGL
+
